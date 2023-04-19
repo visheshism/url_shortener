3f0c592751daffc0109a31470055edbea11dd870
Vishesh Singh <67804974+visheshism@users.noreply.github.com>
2023-04-19 10:26:21 +0530

    fixed missing identifier and link in new callback function createUrl.js

--- middlewares/createUrl.js
+++ middlewares/createUrl.js
@@ -1,6 +1,8 @@
 import { Link } from "../models/shorten.js"
 
 export const createLink = (req, res, next) => {
+    const { identifier } = req.params
+    const { link } = req.body
     const createLink = await Link.create({ identifier, absolute_path: req.protocol + '://' + req.get('host') + '/' + identifier, link })
     const findLink = await Link.findOne({ identifier: createLink.identifier }).select({ _id: 0, __v: 0 })
     res.status(500).json({
